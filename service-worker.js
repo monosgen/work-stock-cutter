742c25a5bcef48646fbd8dee7f289cc34cfb8ed7
monosgen <163592594+monosgen@users.noreply.github.com>
2025-12-15 17:45:30 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE = "wsc-cache-v2";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./service-worker.js"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(
+      (res) =>
+        res ||
+        fetch(e.request)
+          .then((net) => {
+            const copy = net.clone();
+            caches
+              .open(CACHE)
+              .then((c) => c.put(e.request, copy))
+              .catch(() => {});
+            return net;
+          })
+          .catch(() => res)
+    )
+  );
+});
