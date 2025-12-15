80f07a20c0475240df381d7085bf20be5f07624f
monosgen <163592594+monosgen@users.noreply.github.com>
2025-12-15 19:00:09 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "wsc-cache-v6";
+const CACHE = "wsc-cache-v6.1";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./service-worker.js"];
 
 self.addEventListener("install", (e) => {
